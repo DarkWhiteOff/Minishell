9b69f6c2cb760c2e313800806745833782fff30e
Ziggy Amgar <zamgar@bess-f4r6s5.clusters.42paris.fr>
2025-05-01 10:30:34 +0200

    YES

--- src/parsing/check2.c
+++ src/parsing/check2.c
@@ -6,7 +6,7 @@
 /*   By: zamgar <zamgar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 00:07:16 by tzizi             #+#    #+#             */
-/*   Updated: 2025/02/07 18:51:35 by zamgar           ###   ########.fr       */
+/*   Updated: 2025/02/07 21:44:24 by zamgar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ int	is_cmd(char *s, t_main *main)
 
 	i = -1;
 	tmp = NULL;
-	if (!s)
+	if (check_no_path(s, main) == 1)
 		return (0);
 	if (check_first_cmd(s, main) == 1)
 		return (1);
--- src/parsing/t_cmd_utils.c
+++ src/parsing/t_cmd_utils.c
@@ -12,22 +12,6 @@
 
 #include "../../includes/minishell.h"
 
-void	print_t_cmd(t_cmd *cmd)
-{
-	if (!cmd)
-		printf("no token\n");
-	if (!cmd)
-		return ;
-	while (cmd->next != NULL)
-	{
-		printf("fdin %d fdout %d heredoc '%s' cmd '%s' args '%s'\n",
-			cmd->infile, cmd->outfile, cmd->heredoc_eof, cmd->cmd, cmd->args);
-		cmd = cmd->next;
-	}
-	printf("fdin %d fdout %d heredoc '%s' cmd '%s' args '%s'\n", cmd->infile,
-		cmd->outfile, cmd->heredoc_eof, cmd->cmd, cmd->args);
-}
-
 void	ft_lstadd_front(t_cmd **lst, t_cmd *new)
 {
 	t_cmd	*temp;
